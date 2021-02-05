1c6a220ebc20798232789f1c8d62e02aa47ec6f5
GameurDuXVI <gameur.codeur@gmail.com>
2021-02-05 20:00:17 +0100

    Fix Scoreboard

--- src/main/java/eu/builderscoffee/hub/board/BBBoard.java
+++ src/main/java/eu/builderscoffee/hub/board/BBBoard.java
@@ -12,21 +12,24 @@ public class BBBoard {
     private static final MessageConfiguration messages = Main.getInstance().getMessageConfiguration();
     public static Map<UUID, FastBoard> boards = new HashMap<>();
 
+    private static int amountPlayers = 0;
+
     public static void updateBoard(FastBoard board) {
         final Player player = board.getPlayer();
         eu.builderscoffee.api.Main.getBungeeChannelApi().getPlayerCount("ALL")
                 .whenComplete((result, error) -> {
-                    List<String> lines = new ArrayList<>();
-                    final String primaryGroup = LuckPermsUtils.getPrimaryGroup(player).substring(0, 1).toUpperCase() + LuckPermsUtils.getPrimaryGroup(player).substring(1);
-                    messages.getScoreBoard().forEach(line -> lines.add(line
-                            .replace("%player%", player.getName())
-                            .replace("%online%", "" + result)
-                            .replace("%rank%", primaryGroup)
-                            .replace("%prefix%", LuckPermsUtils.getPrefix(player))
-                            .replace("%suffix%", LuckPermsUtils.getSuffix(player))
-                            .replace("&", "§")
-                    ));
-                    board.updateLines(lines);
+                    amountPlayers = result;
                 });
+        List<String> lines = new ArrayList<>();
+        final String primaryGroup = LuckPermsUtils.getPrimaryGroup(player).substring(0, 1).toUpperCase() + LuckPermsUtils.getPrimaryGroup(player).substring(1);
+        messages.getScoreBoard().forEach(line -> lines.add(line
+                .replace("%player%", player.getName())
+                .replace("%online%", "" + amountPlayers)
+                .replace("%rank%", primaryGroup)
+                .replace("%prefix%", LuckPermsUtils.getPrefix(player))
+                .replace("%suffix%", LuckPermsUtils.getSuffix(player))
+                .replace("&", "§")
+        ));
+        board.updateLines(lines);
     }
 }
